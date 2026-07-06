b435978427ff839e4a707aac637cb8fc8693d158
Curtis Pene <curtispene92@gmail.com>
2026-07-07 05:28:05 +1200

    Fix mobile navbar hidden below viewport by using 100dvh instead of 100vh

--- client/src/app/components/RootLayout.tsx
+++ client/src/app/components/RootLayout.tsx
@@ -3,7 +3,7 @@ import { MobileShell } from "./mobileShell/MobileShell";
 
 export const RootLayout = () => {
   return (
-    <main style={{ position: "fixed", height: "100vh", width: "100vw" }}>
+    <main style={{ position: "fixed", height: "100dvh", width: "100vw" }}>
       <MobileShell />
       <DesktopShell />
     </main>
